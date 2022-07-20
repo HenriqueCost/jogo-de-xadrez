d40e0533e3e3469815a0e96295f683ee63b295b5
Henrique Costa <henrique.costa@ibm.com>
2022-07-20 10:41:55 -0300

    Camada de xadrez implementada e impressao do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
-import jogoDeTabuleiro.Posicao;
+import xadrez.PartidaDeXadrez;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Posicao pos = new Posicao(3, 5);
-		System.out.println(pos);
+		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
+		InterfaceDeUsuario.imprimirTabuleiro(partidaDeXadrez.getPecas());
 	}
 
 }
